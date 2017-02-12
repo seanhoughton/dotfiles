e0ccce89849a7c773a7e55561013250529697676
Sean Houghton <sean.houghton@activision.com>
2017-02-12 10:01:20 -0800

    Add sublime text package setup

--- sublime/install.sh
+++ sublime/install.sh
@@ -2,20 +2,20 @@
 set -e
 
 if [ "$(uname -s)" == "Darwin" ]; then
-	prefix="$HOME/Library/Application Support/Sublime Text 3/Packages/User"
+	prefix="$HOME/Library/Application Support/Sublime Text 3"
 	keymap="Default (OSX).sublime-keymap"
 else
-	prefix="$HOME/.config/sublime-text-3/Packages/User"
+	prefix="$HOME/.config/sublime-text-3"
 	keymap="Default (Linux).sublime-keymap"
 fi
 
 
 function symlink_preference() {
 	name=$1
-	original="$prefix/$name"
+	original="$prefix/Packages/User/$name"
 	new="$HOME/.dotfiles/sublime/$name"
 	if [ -f "$original" ]; then
-     	mv "$original" "${original}.backup"
+     	rm "$original"
 	fi
 	if [ ! -h "$original" ]; then
 		ln -s "$new" "$original"
@@ -23,7 +23,12 @@ function symlink_preference() {
 }
 
 echo 'Linking SublimeText preferences'
+if [ ! -f "$prefix/Installed Packages/Package Control.sublime-package" ]; then
+	curl -s -f "https://packagecontrol.io/Package%20Control.sublime-package" > "$prefix/Installed Packages/Package Control.sublime-package"
+fi
 symlink_preference 'Preferences.sublime-settings'
 symlink_preference 'Anaconda.sublime-settings'
+symlink_preference 'Package Control.sublime-settings'
 symlink_preference "$keymap"
+
 
