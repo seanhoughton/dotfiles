e123e48e5804d8bad7adc63316b27b18b991f76a
Sean Houghton <sean.houghton@activision.com>
2017-02-12 09:07:02 -0800

    Add VSCode settings

---
+++ vscode/install.sh
@@ -0,0 +1,25 @@
+#!/usr/bin/env bash
+
+set -e
+
+if [ "$(uname -s)" == "Darwin" ]; then
+	prefix="$HOME/Library/Application Support/Code/User"
+else
+	prefix="$HOME/.config/Code/User"
+fi
+
+
+function symlink_preference() {
+	name=$1
+	original="$prefix/$name"
+	new="$HOME/.dotfiles/vscode/$name"
+	if [ -f "$original" ]; then
+     	mv "$original" "${original}.backup"
+	fi
+	if [ ! -h "$original" ]; then
+		ln -s "$new" "$original"
+	fi
+}
+
+echo 'Linking Visual Studio Code preferences'
+symlink_preference 'settings.json'
