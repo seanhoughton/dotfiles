c10fb2b9ff6e59a666b7cf6ddee9f44d5403f210
Sean Houghton <sean.houghton@gmail.com>
2017-02-11 19:41:11 -0800

    Don't add apt repos on Darwin

--- apt/install.sh
+++ apt/install.sh
@@ -1,4 +1,9 @@
-repofile=$(dirname $0)/repositories.lst
-while read repo; do
-	sudo apt-add-repository -y $repo
-done < $repofile
+#!/usr/bin/env bash
+set -e
+
+if [ "$(uname -s)" == "Linux" ]; then
+	repofile=$(dirname $0)/repositories.lst
+	while read repo; do
+		sudo apt-add-repository -y $repo
+	done < $repofile
+fi
