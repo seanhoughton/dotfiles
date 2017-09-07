d0be704bcc15c83e8b8c1052f4efe75b1ac3aaa0
Sean Houghton <sean.houghton@activision.com>
2017-09-07 09:28:39 -0700

    Add p4 aliass for activision

--- zsh/aliases.zsh
+++ zsh/aliases.zsh
@@ -3,3 +3,10 @@ alias reload!='. ~/.zshrc'
 alias cls='clear' # Good 'ol Clear Screen command
 
 alias colourify='grc /usr/local/bin/grc -es --colour=auto' # fix broken alias
+
+alias p4ct='p4 -p perforce.ct.activision.com:1666 -u shoughton'
+alias p4ctadmin='p4 -p perforce.ct.activision.com:1666 -u p4ctadmin'
+alias p4ctdev='p4 -p perforce01.ctdev.activision.com:1666 -u p4admin'
+alias p4iw='p4 -p p4-replica-02.infinityward.net:1666 -u shoughton'
+alias p4ta='p4 -p bundy.activision.com:1666 -u shoughton'
+alias p4shg='p4 -p tango.activision.com:1666 -u shoughton'
